2bdff086215dae265f00c57b360c8e7d84d65ab0
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-02 19:11:24 +0300

    feature:doc product model

--- src/models/product.js
+++ src/models/product.js
@@ -1,19 +1,43 @@
+/**
+ * The data-layer for a product
+ * @module product
+ * @requires mongoose
+ */
 /* ------------ importing packages ------------ */
+/**
+ * mongoose module to create collections in the database
+ * @type {Mongoose}
+ * @const
+ * @namespace mongoose
+ */
 const mongoose = require('mongoose');
 
-/**
- * @description creating the product schema using mongoose.
- */
+
 /* ------------ Creating Schema ------------ */
+
+/**
+ *  product schema
+ * @constructor Language
+ */
+
 const ProductSchema = mongoose.Schema({
-	name: { type: String },
-	quantity: { type: Number },
-	code: { type: String },
-	price: { type: Number },
+    //name: product name
+    name: {type: String},
+    //quantity: quantity of product
+    quantity: {type: Number},
+    //code: code of product
+    code: {type: String},
+    //price: price of product
+    price: {type: Number},
 });
 
 /* ------------ Creating Mongo model ------------ */
 const Product = mongoose.model('Product', ProductSchema);
 
 /* ------------ exporting models  ------------ */
+/**
+ * this module exports product model
+ * @exports
+ * @type {Object<Product>}
+ */
 module.exports = Product;
