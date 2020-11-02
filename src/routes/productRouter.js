0b2547c1f45e90170aff60a25eaf0d7e0940a7ba
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-02 19:28:12 +0300

    feature: doc product route

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,88 +1,165 @@
+/** Express router providing product related routes
+ * @module routes/productRouter
+ * @requires express
+ * @requires validate
+ * @requires validationResult
+ * @requires productController
+ */
 /* ------- importing Packages -------- */
+
 const productController = require('../controllers/productController');
+/**
+ * express module
+ * @const
+ * @namespace express
+ */
+
 const express = require('express');
+
+/**
+ * Express router to mount word related functions on.
+ * @type {object}
+ * @const
+ * @namespace productRouter
+ */
 const productRouter = express.Router();
+/**
+ *  validate object has functions to validate requests before going to mess with database.
+ * @type {object}
+ * @const
+ * @namespace validate
+ */
+
 const validate = require('../utils/productValidators');
-const { validationResult } = require('express-validator/check');
+
+/**
+ *  express validation result object holds the result after validation a request using express validator.
+ * @const
+ * @type {Object}
+ * @namespace validationResult
+ */
+const {validationResult} = require('express-validator/check');
+
 
 /* ------- products routes -------- */
 
 /**
- * 'get' route where it has two cases
- * FIRST CASE : gets all the products from the database if it is not includes a query in the url as /products/?code=1234
- * SECOND CASE : gets a particular product if query was included in the request as /products/?code=1234
+ * Route getting all products.
+ * @name get/products
+ * @function
+ * @memberOf module:routes/productRouter~route
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express validator middleware.
+ * @param {callback} middleware - Express middleware.
  */
 productRouter.get('/', validate('getProducts'), async (req, res) => {
-	var err = validationResult(req);
-	if (!err.isEmpty()) {
-		res.send(err.mapped()).status(400);
-	} else {
-		//if the user want by code
-		if (req.query.code) {
-			const code = req.query.code;
-			const response = await productController.getProductByCode(code);
-			res.json(response).status(200);
-		} else {
-			const response = await productController.getProducts();
-			res.json(response).status(200);
-		}
-	}
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        //FIRST CASE : gets all the products from the database if it is not includes a query in the url as /products/?code=1234
+        //if the user want by code
+        if (req.query.code) {
+            const code = req.query.code;
+            const response = await productController.getProductByCode(code);
+            res.json(response).status(200);
+            // SECOND CASE : gets a particular product if query was included in the request as /products/?code=1234
+        } else {
+            const response = await productController.getProducts();
+            res.json(response).status(200);
+        }
+    }
 });
 
 /**
- * 'get' route where it gets a particular product by id
+ * Route getting product by id.
+ * @name get/products/:id
+ * @function
+ * @memberOf module:routes/productRouter~route
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express validator middleware.
+ * @param {callback} middleware - Express middleware.
  */
+
 productRouter.get('/:id', validate('getProductById'), async (req, res) => {
-	var err = validationResult(req);
-	if (!err.isEmpty()) {
-		res.send(err.mapped()).status(400);
-	} else {
-		const id = req.params.id;
-		const response = await productController.getProductById(id);
-		res.json(response).status(200);
-	}
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const id = req.params.id;
+        const response = await productController.getProductById(id);
+        res.json(response).status(200);
+    }
 });
 
 /**
- * 'post' route where it posts a new product to the database
+ * Route post new product.
+ * @name post/products
+ * @function
+ * @memberOf module:routes/productRouter~route
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express validator middleware.
+ * @param {callback} middleware - Express middleware.
  */
 productRouter.post('/', validate('postProduct'), async (req, res) => {
-	var err = validationResult(req);
-	if (!err.isEmpty()) {
-		res.send(err.mapped()).status(400);
-	} else {
-		const response = await productController.postProduct(req.body);
-		res.json(response).status(200);
-	}
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const response = await productController.postProduct(req.body);
+        res.json(response).status(200);
+    }
 });
 
 /**
- * 'put' route where it updates particular product information from the database
+ * Route updating product by id.
+ * @name put/products/:id
+ * @function
+ * @memberOf module:routes/productRouter~route
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express validator middleware.
+ * @param {callback} middleware - Express middleware.
  */
 productRouter.put('/:id', validate('putProduct'), async (req, res) => {
-	var err = validationResult(req);
-	if (!err.isEmpty()) {
-		res.send(err.mapped()).status(400);
-	} else {
-		const id = req.params.id;
-		const response = await productController.putProduct(id, req.body);
-		res.json(response).status(200);
-	}
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const id = req.params.id;
+        const response = await productController.putProduct(id, req.body);
+        res.json(response).status(200);
+    }
 });
 
 /**
- * 'delete' route where it deletes a particular product information
+ * Route deleting product by id.
+ * @name delete/products/:id
+ * @function
+ * @memberOf module:routes/productRouter~route
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express validator middleware.
+ * @param {callback} middleware - Express middleware.
  */
 productRouter.delete('/:id', validate('deleteProduct'), async (req, res) => {
-	var err = validationResult(req);
-	if (!err.isEmpty()) {
-		res.send(err.mapped()).status(400);
-	} else {
-		const id = req.params.id;
-		const response = await productController.deleteProduct(id);
-		res.json(response).status(200);
-	}
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const id = req.params.id;
+        const response = await productController.deleteProduct(id);
+        res.json(response).status(200);
+    }
 });
 
 /* ------- model exporting -------- */
+/**
+ * this module exports the products route endpoints
+ * @exports
+ * @type {Object}
+ */
 module.exports = productRouter;
