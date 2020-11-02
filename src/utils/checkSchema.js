3651ead5d96eb0040154aedaab6cce01ac53eb39
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-02 19:32:20 +0300

    feature:validating utils folder

--- src/utils/checkSchema.js
+++ src/utils/checkSchema.js
@@ -1,63 +1,71 @@
-/* eslint-disable indent */
-/*          ------------------- EXPLANATION -------------------
- since all the request must has validation schema a separate file created to check the schema of a request;
-*/
-
-/* ------------------- functions ------------------- */
+/**
+ * since all the request must has validation schema a separate file created to check the schema of a request
+ * @module utils/checkSchema
+ */
 
 /** @author Nawaf Alsharqi
  * @export
  * @function
  * @name validateSchema.
- * @param {list}  schemas - list hold all the schema of a particular request.
+ * @param {Array<String>}  schemas - list hold all the schema of a particular request.
  * @param {Object} request - request Object that holds the request body.
  * @returns {boolean} if there is no error.
- * @returns {Error} if there is an error.
+ * @throws {Error} if there is an error.
  * @description validates the schema of a request.
  */
-
 function validateSchema(schemas, request) {
-	//the purpose of the unRequiredSchemas is to add any key from the request body inside it;
-	let unRequiredSchemas = [];
+    //the purpose of the unRequiredSchemas is to add any key from the request body inside it;
+    let unRequiredSchemas = [];
 
-	//looping throw the object keys
-	for (let [key, value] of Object.entries(request.body)) {
-		//checking if the key is not in the schema
-		if (!schemas.includes(key)) {
-			//if there is error we push the key into the list
-			unRequiredSchemas.push(key);
-		}
-	}
-	//if there is an error throw with all unRequired schemas else return  by checking if the unRequiredSchemas length is zero;;
-	if (unRequiredSchemas.length == 0) return true;
-	else throw new Error(`invalid queries [${unRequiredSchemas}] were included in the request`);
+    //looping throw the object keys
+    for (let [key, value] of Object.entries(request.body)) {
+        //checking if the key is not in the schema
+        if (!schemas.includes(key)) {
+            //if there is error we push the key into the list
+            unRequiredSchemas.push(key);
+        }
+    }
+    //if there is an error throw with all unRequired schemas else return  by checking if the unRequiredSchemas length is zero;;
+    if (unRequiredSchemas.length === 0) {
+        return true;
+    } else {
+        throw new Error(`invalid queries [${unRequiredSchemas}] were included in the request`);
+    }
 }
 
 /** @author Nawaf Alsharqi
  * @export
  * @function
  * @name validateQuery.
- * @param {list}  queries - list hold all the schema of a particular request.
+ * @param {Array<String>}  queries - list hold all the schema of a particular request.
  * @param {Object} request - request Object that holds the request query.
  * @returns {boolean} if there is no error.
- * @returns {Error} if there is an error.
+ * @throws {Error} if there is an error.
  * @description validates the query of a request url
  */
 function validateQuery(queries, request) {
-	//the purpose of the unRequiredSchemas is to add any key from the request body inside it;
-	let unRequiredQueries = [];
+    //the purpose of the unRequiredSchemas is to add any key from the request body inside it;
+    let unRequiredQueries = [];
 
-	//looping throw the object keys
-	for (let [key, value] of Object.entries(request.query)) {
-		//checking if the key is not in the schema
-		if (!queries.includes(key)) {
-			//if there is error we push the key into the list
-			unRequiredQueries.push(key);
-		}
-	}
-	//if there is an error throw with all unRequired schemas else return  by checking if the unRequiredSchemas length is zero;;
-	if (unRequiredQueries.length == 0) return true;
-	else throw new Error(`invalid queries [${unRequiredQueries}] were included in the request`);
+    //looping throw the object keys
+    for (let [key, value] of Object.entries(request.query)) {
+        //checking if the key is not in the schema
+        if (!queries.includes(key)) {
+            //if there is error we push the key into the list
+            unRequiredQueries.push(key);
+        }
+    }
+    //if there is an error throw with all unRequired schemas else return  by checking if the unRequiredSchemas length is zero;;
+    if (unRequiredQueries.length === 0) {
+        return true;
+    } else {
+        throw new Error(`invalid queries [${unRequiredQueries}] were included in the request`);
+    }
 }
 
-module.exports = { validateSchema, validateQuery };
+/**
+ * module that contains functions to validate schema and quires
+ * @exports
+ * @type {function(Array<String>, Object): Error,function(Array<String>,Object):Error}
+ */
+module.exports = {validateSchema, validateQuery};
--- src/utils/productValidators.js
+++ src/utils/productValidators.js
@@ -1,8 +1,33 @@
+/**
+ *  since all the routes are following the RESTFUL API naming so based on the name of the function in the route
+ * will goes into the switch case as a case.
+ * @module utils/productValidators
+ * @requires body
+ * @requires param
+ * @requires validator
+ */
+
 /* ------- importing Packages -------- */
-const { body, param, query } = require('express-validator/check');
+/**
+ * body and param of type express validator module to check the request param and body
+ * @type {Object}
+ * @const
+ * @namespace body
+ * @namespace param
+ * @namespace query
+ */
+const {body, param, query} = require('express-validator/check');
+/**
+ * Object that holds functions to validate request schema and queries of giving request
+ * @type {Object}
+ * @const
+ * @namespace validator
+ */
 const validator = require('./checkSchema');
-const { request } = require('express');
-/** @author Nawaf Alsharqi. 
+
+
+/* ------- Functions -------- */
+/** @author Nawaf Alsharqi.
  * @function
  * @name validate
  * @param {String} method name of the case to determine which validation we go with.
@@ -11,213 +36,218 @@ const { request } = require('express');
  */
 
 const validate = (method) => {
-	switch (method) {
-		case 'getProducts': {
-			return [
-				/* ----------------- Schema Validation -----------------*/
-				body(' ').custom((value, { req }) => {
-					const schemas = [undefined];
-					if (validator.validateSchema(schemas, req)) {
-						return true;
-					}
-				}),
+    switch (method) {
+        case 'getProducts': {
+            return [
+                /* ----------------- Schema Validation -----------------*/
+                body(' ').custom((value, {req}) => {
+                    const schemas = [undefined];
+                    if (validator.validateSchema(schemas, req)) {
+                        return true;
+                    }
+                }),
 
-				/* ----------------- END Schema Validation -----------------*/
+                /* ----------------- END Schema Validation -----------------*/
 
-				/* ----------------- Query Validation -----------------*/
-				query(' ').custom((value, { req }) => {
-					const queries = ['code'];
-					if (validator.validateQuery(queries, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- End Query Validation -----------------*/
-			];
-		}
-		case 'getProductById': {
-			return [
-				/* ----------------- ID Validation -----------------*/
-				param('id', 'id must be exists and of type string and match mongoID pattern')
-					.exists()
-					.isMongoId(),
-					
-				/* ----------------- Query Validation -----------------*/
-				query(' ').custom((value, { req }) => {
-					const queries = [undefined];
-					if (validator.validateQuery(queries, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- End Query Validation ----------------- */
+                /* ----------------- Query Validation -----------------*/
+                query(' ').custom((value, {req}) => {
+                    const queries = ['code'];
+                    if (validator.validateQuery(queries, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- End Query Validation -----------------*/
+            ];
+        }
+        case 'getProductById': {
+            return [
+                /* ----------------- ID Validation -----------------*/
+                param('id', 'id must be exists and of type string and match mongoID pattern')
+                    .exists()
+                    .isMongoId(),
 
-				/* ----------------- Schema Validation -----------------*/
-				body(' ').custom((value, { req }) => {
-					const schemas = [undefined];
-					if (validator.validateSchema(schemas, req)) {
-						return true;
-					}
-				}),
+                /* ----------------- Query Validation -----------------*/
+                query(' ').custom((value, {req}) => {
+                    const queries = [undefined];
+                    if (validator.validateQuery(queries, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- End Query Validation ----------------- */
 
-				/* ----------------- END Schema Validation -----------------*/
-			];
-		}
+                /* ----------------- Schema Validation -----------------*/
+                body(' ').custom((value, {req}) => {
+                    const schemas = [undefined];
+                    if (validator.validateSchema(schemas, req)) {
+                        return true;
+                    }
+                }),
 
-		case 'postProduct': {
-			return [
-				/* ----------------- Schema Validation -----------------*/
-				body(' ').custom((value, { req }) => {
-					const schemas = ['name', 'quantity', 'code', 'price'];
-					if (validator.validateSchema(schemas, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- END Schema Validation -----------------*/
+                /* ----------------- END Schema Validation -----------------*/
+            ];
+        }
 
-				/* ----------------- Query Validation -----------------*/
-				query(' ').custom((value, { req }) => {
-					const queries = [undefined];
-					if (validator.validateQuery(queries, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- End Query Validation ----------------- */
+        case 'postProduct': {
+            return [
+                /* ----------------- Schema Validation -----------------*/
+                body(' ').custom((value, {req}) => {
+                    const schemas = ['name', 'quantity', 'code', 'price'];
+                    if (validator.validateSchema(schemas, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- END Schema Validation -----------------*/
 
-				/* ----------------- Name Validation ----------------- */
-				body('name', 'name must be exists and be of type String').exists().isString(),
-				body('name', 'name of product cannot be empty string').not().equals(''),
-				body('name', 'name product cannot be empty string').not().equals(' '),
-				/* ----------------- End  name Validation -----------------*/
+                /* ----------------- Query Validation -----------------*/
+                query(' ').custom((value, {req}) => {
+                    const queries = [undefined];
+                    if (validator.validateQuery(queries, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- End Query Validation ----------------- */
 
-				/* ----------------- quantity Validation ----------------- */
-				body('quantity', 'quantity must be of type integer').optional().isInt(),
-				body('quantity')
-					.optional()
-					.custom((value, { req }) => {
-						if (value < 0) {
-							throw Error('quantity cannot be less than zero');
-						} else {
-							return true;
-						}
-					}),
-				/* ----------------- end quantity Validation ----------------- */
+                /* ----------------- Name Validation ----------------- */
+                body('name', 'name must be exists and be of type String').exists().isString(),
+                body('name', 'name of product cannot be empty string').not().equals(''),
+                body('name', 'name product cannot be empty string').not().equals(' '),
+                /* ----------------- End  name Validation -----------------*/
 
-				/* ----------------- code Validation ----------------- */
-				body('code', 'code must exists and be of type string').exists().isString(),
-				body('code', 'code cannot be empty string').not().equals(''),
-				body('code', 'code cannot be empty string').not().equals(' '),
+                /* ----------------- quantity Validation ----------------- */
+                body('quantity', 'quantity must be of type integer').optional().isInt(),
+                body('quantity')
+                    .optional()
+                    .custom((value, {req}) => {
+                        if (value < 0) {
+                            throw Error('quantity cannot be less than zero');
+                        } else {
+                            return true;
+                        }
+                    }),
+                /* ----------------- end quantity Validation ----------------- */
 
-				/* ----------------- end code Validation ----------------- */
+                /* ----------------- code Validation ----------------- */
+                body('code', 'code must exists and be of type string').exists().isString(),
+                body('code', 'code cannot be empty string').not().equals(''),
+                body('code', 'code cannot be empty string').not().equals(' '),
 
-				/* ----------------- price Validation ----------------- */
-				body('price', 'price must be of type number').optional().isNumeric(),
+                /* ----------------- end code Validation ----------------- */
 
-				body('price')
-					.optional()
-					.custom((value, { req }) => {
-						if (value < 0) {
-							throw Error('price cannot be less than zero');
-						} else {
-							return true;
-						}
-					}),
-				/* ----------------- end price Validation ----------------- */
-			];
-		}
+                /* ----------------- price Validation ----------------- */
+                body('price', 'price must be of type number').optional().isNumeric(),
 
-		case 'putProduct': {
-			return [
-				/* ----------------- ID Validation -----------------*/
-				param('id', 'id must be exists and of type string and match mongoID pattern')
-					.exists()
-					.isMongoId(),
+                body('price')
+                    .optional()
+                    .custom((value, {req}) => {
+                        if (value < 0) {
+                            throw Error('price cannot be less than zero');
+                        } else {
+                            return true;
+                        }
+                    }),
+                /* ----------------- end price Validation ----------------- */
+            ];
+        }
 
-				/* ----------------- Schema Validation -----------------*/
-				body(' ').custom((value, { req }) => {
-					const schemas = ['name', 'quantity', 'code', 'price'];
-					if (validator.validateSchema(schemas, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- END Schema Validation -----------------*/
+        case 'putProduct': {
+            return [
+                /* ----------------- ID Validation -----------------*/
+                param('id', 'id must be exists and of type string and match mongoID pattern')
+                    .exists()
+                    .isMongoId(),
 
-				/* ----------------- Query Validation -----------------*/
-				query(' ').custom((value, { req }) => {
-					const queries = [undefined];
-					if (validator.validateQuery(queries, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- End Query Validation ----------------- */
+                /* ----------------- Schema Validation -----------------*/
+                body(' ').custom((value, {req}) => {
+                    const schemas = ['name', 'quantity', 'code', 'price'];
+                    if (validator.validateSchema(schemas, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- END Schema Validation -----------------*/
 
-				/* ----------------- Name Validation ----------------- */
-				body('name', 'name must be exists and be of type String').optional().isString(),
-				body('name', 'name of product cannot be empty string').optional().not().equals(''),
-				body('name', 'name product cannot be empty string').optional().not().equals(' '),
-				/* ----------------- End  name Validation -----------------*/
+                /* ----------------- Query Validation -----------------*/
+                query(' ').custom((value, {req}) => {
+                    const queries = [undefined];
+                    if (validator.validateQuery(queries, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- End Query Validation ----------------- */
 
-				/* ----------------- quantity Validation ----------------- */
-				body('quantity', 'quantity must be of type integer').optional().isInt(),
-				body('quantity')
-					.optional()
-					.custom((value, { req }) => {
-						if (value < 0) {
-							throw Error('quantity cannot be less than zero');
-						} else {
-							return true;
-						}
-					}),
-				/* ----------------- end quantity Validation ----------------- */
+                /* ----------------- Name Validation ----------------- */
+                body('name', 'name must be exists and be of type String').optional().isString(),
+                body('name', 'name of product cannot be empty string').optional().not().equals(''),
+                body('name', 'name product cannot be empty string').optional().not().equals(' '),
+                /* ----------------- End  name Validation -----------------*/
 
-				/* ----------------- code Validation ----------------- */
-				body('code', 'code must exists and be of type string').optional().exists().isString(),
-				body('code', 'code cannot be empty string').optional().not().equals(''),
-				body('code', 'code cannot be empty string').optional().not().equals(' '),
+                /* ----------------- quantity Validation ----------------- */
+                body('quantity', 'quantity must be of type integer').optional().isInt(),
+                body('quantity')
+                    .optional()
+                    .custom((value, {req}) => {
+                        if (value < 0) {
+                            throw Error('quantity cannot be less than zero');
+                        } else {
+                            return true;
+                        }
+                    }),
+                /* ----------------- end quantity Validation ----------------- */
 
-				/* ----------------- end code Validation ----------------- */
+                /* ----------------- code Validation ----------------- */
+                body('code', 'code must exists and be of type string').optional().exists().isString(),
+                body('code', 'code cannot be empty string').optional().not().equals(''),
+                body('code', 'code cannot be empty string').optional().not().equals(' '),
 
-				/* ----------------- price Validation ----------------- */
-				body('price', 'price must be of type number').optional().isNumeric(),
+                /* ----------------- end code Validation ----------------- */
 
-				body('price')
-					.optional()
-					.custom((value, { req }) => {
-						if (value < 0) {
-							throw Error('price cannot be less than zero');
-						} else {
-							return true;
-						}
-					}),
-				/* ----------------- end price Validation ----------------- */
-			];
-		}
-		case 'deleteProduct': {
-			return [
-				/* ----------------- ID Validation -----------------*/
-				param('id', 'id must be exists and of type string and match mongoID pattern')
-					.exists()
-					.isMongoId(),
+                /* ----------------- price Validation ----------------- */
+                body('price', 'price must be of type number').optional().isNumeric(),
 
-				/* ----------------- Query Validation -----------------*/
-				query(' ').custom((value, { req }) => {
-					const queries = [undefined];
-					if (validator.validateQuery(queries, req)) {
-						return true;
-					}
-				}),
-				/* ----------------- End Query Validation ----------------- */
+                body('price')
+                    .optional()
+                    .custom((value, {req}) => {
+                        if (value < 0) {
+                            throw Error('price cannot be less than zero');
+                        } else {
+                            return true;
+                        }
+                    }),
+                /* ----------------- end price Validation ----------------- */
+            ];
+        }
+        case 'deleteProduct': {
+            return [
+                /* ----------------- ID Validation -----------------*/
+                param('id', 'id must be exists and of type string and match mongoID pattern')
+                    .exists()
+                    .isMongoId(),
 
-				/* ----------------- Schema Validation -----------------*/
-				body(' ').custom((value, { req }) => {
-					const schemas = [undefined];
-					if (validator.validateSchema(schemas, req)) {
-						return true;
-					}
-				}),
+                /* ----------------- Query Validation -----------------*/
+                query(' ').custom((value, {req}) => {
+                    const queries = [undefined];
+                    if (validator.validateQuery(queries, req)) {
+                        return true;
+                    }
+                }),
+                /* ----------------- End Query Validation ----------------- */
 
-				/* ----------------- END Schema Validation -----------------*/
-			];
-		}
-	}
+                /* ----------------- Schema Validation -----------------*/
+                body(' ').custom((value, {req}) => {
+                    const schemas = [undefined];
+                    if (validator.validateSchema(schemas, req)) {
+                        return true;
+                    }
+                }),
+
+                /* ----------------- END Schema Validation -----------------*/
+            ];
+        }
+    }
 };
 /* ------- model exporting -------- */
+/**
+ * this module exports the validate method to validate body request
+ * @exports
+ * @type {function(String): ([this, this]|[this, this, this]|*|[this, this, this])}
+ */
 module.exports = validate;
