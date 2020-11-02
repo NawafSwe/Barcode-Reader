79431b7b7f1deb732594b2614fd431df8249583f
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-02 19:06:46 +0300

    feature : documenting index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
+/** index module to config express app server
+ * @module src/index
+ * @requires express
+ * @requires bodyParser
+ * @requires cors
+ * @requires db_connection
+ */
+
 /* ------- importing Packages -------- */
+/**
+ * express module
+ * @const
+ * @namespace express
+ */
 
 const express = require('express');
+
+/**
+ * body parser model
+ * @const
+ * @type {Object}
+ * @namespace bodyParser
+ */
+
 const bodyParser = require('body-parser');
+
+/**
+ * cors module
+ * @type {Object}
+ * @const
+ * @namespace cors
+ */
+
 const cors = require('cors');
-const db_connection = require('./db');
-const productRouter = require('./routes/productRouter');
+
+/**
+ * Object has function to connect to a mongoDB
+ * @type {Object}
+ * @const
+ * @namespace db_connection
+ */
+const db_connection = require('./configuration/db');
+
+
+
 
 /* -------App Configuration-------- */
 const app = express();
@@ -15,9 +53,9 @@ app.use(express.static('public'));
 
 /* -------------- choosing Env ---------------------- */
 if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
-	require('custom-env').env(process.env.NODE_ENV);
+    require('custom-env').env(process.env.NODE_ENV);
 } else {
-	require('dotenv').config();
+    require('dotenv').config();
 }
 
 /* -------------- Database Connection ---------------------- */
@@ -25,11 +63,30 @@ const MONGO_URI = process.env.MONGO_URI;
 db_connection(MONGO_URI);
 
 /* -------------- checking backend connectivity ---------------------- */
+/**
+ * Route get backend health.
+ * @name get/
+ * @function
+ * @memberOf module:src/index~route
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+
 app.get('/', async (req, res) => {
-	res.render('home');
+    res.render('home');
 });
 
 /* -------------- App Routers---------------------- */
+
+/**
+ * wordRouter to use word route endpoints
+ * @type {Object}
+ * @const
+ * @namespace productRouter
+ */
+
+const productRouter = require('./routes/productRouter');
 app.use('/products', productRouter);
 
 /* -------------- establishing server connection ---------------------- */
