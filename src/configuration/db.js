1a773ebaef0d6749d48b6c21abd5c506cba18159
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-29 11:15:00 +0300

    hotfix:fixing mongoose db options

--- src/configuration/db.js
+++ src/configuration/db.js
@@ -25,16 +25,14 @@ const mongoose = require('mongoose');
  */
 const connection = async (MONGO_URI) => {
     try {
-        const connect_db = await mongoose.connect(MONGO_URI, {
+         await mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
         });
 
         console.log(`successfully connected to barcodeSystem`);
     } catch (error) {
-        console.log(`failed to connect to the database try again later , error : `, e.message);
+        console.log(`failed to connect to the database try again later , error : `, error.message);
     }
 };
 
